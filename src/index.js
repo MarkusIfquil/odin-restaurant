a2222b5509eafbbfaff11474526e7d181b8de061
Mark Nesovanovic <mark.nesovanovic@gmail.com>
2025-08-17 18:41:11 +0200

    added better styling for menu, and added food section

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import "./styles.css";
-
 import { makeHomePage } from "./home";
+import { makeMenuPage } from "./menu";
 
-function showPage() {
+function showPage(div) {
     let root = document.querySelector("#content");
     root.innerHTML = "";
-    root.appendChild(makeHomePage());
+    root.appendChild(div);
 }
 
-showPage();
+let homeButton = document.querySelector("#home");
+let menuButton = document.querySelector("#menu");
+let aboutButton = document.querySelector("#about");
+
+homeButton.onclick = () => {
+    showPage(makeHomePage());
+}
+
+menuButton.onclick = () => {
+    showPage(makeMenuPage());
+}
+
+// homeButton.onclick = () => {
+    // showPage(makeHomePage());
+// }
+
+showPage(makeMenuPage());
--- src/menu.js
+++ src/menu.js
@@ -1,24 +1,28 @@
 function makeSection(title, items) {
     let section = document.createElement("div");
+    section.classList = "section";
     let sectionTitle = document.createElement("h2");
     sectionTitle.textContent = title;
     section.appendChild(sectionTitle);
     for (const item of items) {
         section.appendChild(item);
     }
+    return section;
 }
 
 function makeItem(name, description, price, image) {
     let item = document.createElement("div");
+    item.classList = "card";
     let nameH = document.createElement("h2");
     nameH.textContent = name;
+    item.appendChild(nameH);
+
     let descriptionp = document.createElement("p");
     descriptionp.textContent = description;
-    let pricep = document.createElement("p");
-    pricep = `$${price}`;
-
-    item.appendChild(nameH);
     item.appendChild(descriptionp);
+
+    let pricep = document.createElement("p");
+    pricep.textContent = `$${price}`;
     item.appendChild(pricep);
 
     return item;
@@ -40,8 +44,17 @@ export function makeMenuPage() {
         makeItem("frappe", "iced coffee", 5),
     ];
 
-    let drinksSection = makeSection("drinks", [mocha, macchiato, latte, frappe]);
+    let drinksSection = makeSection("drinks", drinks);
     div.appendChild(drinksSection);
+
+    let food = [
+        makeItem("creppes","soft, buttery creppes with chocolate",5),
+        makeItem("pudding","chocolate or vanilla creamy pudding", 3),
+        makeItem("cat food","NOT for human consumption!", 2),
+    ]
+
+    let foodSection = makeSection("food", food);
+    div.appendChild(foodSection);
 
     return div;
 }
